76c40a12b8ff1bff035faf8feedf4bf7f93b7f66
swogks <wogks3000@gmail.com>
2016-08-08 23:04:27 -0700

    Update image_conversion.sh

--- image_conversion.sh
+++ image_conversion.sh
@@ -4,8 +4,8 @@ i=1
 PATH_NAME=$(pwd) # return current directory path.
 for f in *.MP4 # extension can be changed.
 do
-    echo $f
+    echo $f # show current working video
+    echo $PATH_NAME/cam3_image/$i # show path to save image
     ffmpeg -i $f -qscale:v 31 -vf scale=960:540 $PATH_NAME/cam3_image/$i/%07d.jpg # path can be changed.
-    echo $PATH_NAME/cam3_image/$i
     let i=i+1
 done
